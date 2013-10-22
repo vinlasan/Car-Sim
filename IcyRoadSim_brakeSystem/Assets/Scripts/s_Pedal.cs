f88c80fceb202d0d672fa31fcfc5c73d15a01ef1
Edward Kauffmann <captainredhook@gmail.com>
2013-10-22 12:43:35 -0500

    Brakes and Accelerator GUI system
    Scene that renders the brakes as a secondary camera view in the main
    (player) controller

---
+++ IcyRoadSim_brakeSystem/Assets/Scripts/s_Pedal.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_Pedal : MonoBehaviour {
+	
+	public GameObject Brake;
+	public GameObject Accelerator;
+	
+	public string keyBrake;
+	public string keyAccelerate;
+	
+	public float brakeLimitMax;
+	public float brakeLimitMin;
+	public float acceleratorLimitMax;
+	public float acceleratorLimitMin;
+
+	void Update () {
+		
+		PushPedal();
+	
+	}
+	
+	
+	void PushPedal () {
+		
+		if (Input.GetKeyDown (keyBrake)){
+			if (Brake.transform.position.z < brakeLimitMax)
+			{
+				MovePedal(Brake, true);
+				
+				if (Input.GetKeyUp (keyBrake)){
+					if (Brake.transform.position.z > brakeLimitMin)
+					{
+						MovePedal(Brake, false);
+					}
+				}
+			}
+		}
+		
+		
+		
+		if (Input.GetKeyDown (keyAccelerate)){
+			if (Accelerator.transform.position.z < acceleratorLimitMax)
+			{
+				MovePedal(Accelerator, true);
+				
+				if (Input.GetKeyUp (keyAccelerate)){
+					if (Accelerator.transform.position.z > acceleratorLimitMin)
+					{
+						MovePedal(Brake, false);
+					}
+				}
+			}
+		}
+	}
+	
+	
+	public static void MovePedal (GameObject pedal, bool keyState) {
+		
+		//Vector3 PedalPosition = new Vector3(pedal.transform.position.x, pedal.transform.position.y, pedal.transform.position.z);
+		float MoveSpeed = 1f;
+		float Movement = MoveSpeed * Time.deltaTime;
+		Transform pedalTransform = pedal.transform;
+		
+		if (keyState == true){
+			pedalTransform.Translate(Vector3.forward * Movement);
+		}
+		
+		if (keyState == false){
+			pedalTransform.Translate(Vector3.back * Movement);
+		}
+		
+	}
+}
