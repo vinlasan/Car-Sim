98b2cd2b0b2ce8d6eca0ec4f5354355ff4a8945c
chocobo555 <ian.harris1@loop.colum.edu>
2013-10-23 19:47:29 -0500

    driving/ice overhaul
    I combined the driving and ice scripts. i have added to the driving
    scripts so that when it is on ice it fishtails back and forth. i am
    currently trying to make it so that it only does that while its braking
    on ice.

--- Car_Movement/CarSimulator/Assets/scripts/Ice.cs
+++ Car_Movement/CarSimulator/Assets/scripts/Ice.cs
@@ -3,35 +3,38 @@ using System.Collections;
 
 public class Ice : MonoBehaviour 
 {
-
+	/*
 	GameObject Car;
 	float iceSpeedModifier;
 	
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		Car = GameObject.Find("car");
 		iceSpeedModifier = 0;
 	}
 	
+	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		Car.GetComponent<driving>().drivingSpeed += iceSpeedModifier;
 	}
+	
 	
 	void OnTriggerEnter(Collider c)
 	{
 		print(c.name + "Has entered the trigger");
 		
 		iceSpeedModifier = 0.8f;
-		Car.GetComponent<driving>().turningSpeed = 5f;
-		Car.rigidbody.drag = 1;
 	}
+	
 	
 	void OnTriggerExit()
 	{
 		iceSpeedModifier = 0;
-		Car.GetComponent<driving>().turningSpeed = 30;
 		Car.GetComponent<driving>().drivingSpeed = 30;
-		Car.rigidbody.drag = 3;
+		Car.rigidbody.drag = 4;
 	}
+	*/
 }
--- Car_Movement/CarSimulator/Assets/scripts/driving.cs
+++ Car_Movement/CarSimulator/Assets/scripts/driving.cs
@@ -5,41 +5,109 @@ public class driving : MonoBehaviour
 {
 	public float drivingSpeed;
 	public float turningSpeed = 30;
+	public bool onIce = false;
+	float iceSpeedModifier;
+	public float randomizeTurn = 0;
 	
 
 	// Use this for initialization
-	void Start () { }
+	void Start () 
+	{
+		iceSpeedModifier = 0;
+	}
+	
+	
+	IEnumerator slowDownTurnRand()
+	{
+		while (onIce == true) 
+		{
+			yield return new WaitForSeconds(.5f);
+			if(Random.value < 0.5f)
+			{
+    			randomizeTurn = 20;
+			}
+			else
+			{
+    			randomizeTurn = -20;
+			}
+		}
+	}
 	
 	
 	// Update is called once per frame
 	void Update () 
 	{	
-		print ("Speed " + drivingSpeed);
+		//StartCoroutine("slowDownTurnRand");
+		
+		if (randomizeTurn != 0 && onIce == false) 
+		{
+			randomizeTurn = 0;
+		}
+		
+		//speed modifier while on ice
+		if (drivingSpeed < 25) 
+		{
+			drivingSpeed += iceSpeedModifier;
+		}
 		
 		//move forward
 		if (Input.GetKey("mouse 0")) 
 		{
 			rigidbody.AddForce(transform.forward * drivingSpeed, ForceMode.Acceleration);
-			rigidbody.drag = 3;
 		}
 		
+		//always have acceleration while on ice
+		if (onIce == true) 
+		{
+			rigidbody.AddForce(transform.forward * drivingSpeed, ForceMode.Acceleration);
+		}
+		
+		//acceleration drag
+		if (Input.GetKey("mouse 0") && onIce == false) 
+		{
+			rigidbody.drag = 4;
+		}
 		
 		//turning
 		float turn = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
-		transform.Rotate(0, turn, 0);
+		transform.Rotate(0, turn + randomizeTurn, 0);
 		
 		
 		//neutral
-		if (Input.GetKeyUp("mouse 0"))
+		if (Input.GetKeyUp("mouse 0") && onIce == false)
 		{
 			rigidbody.drag = 1;
 		}
 		
-		
 		//braking
-		if (Input.GetKeyDown("mouse 1")) 
+		if (Input.GetKeyDown("mouse 1") && onIce == false) 
 		{
 			rigidbody.drag = 4.5f;
 		}
+	}
+	
+	
+	void OnTriggerEnter()
+	{	
+		//make sure the update if staments know not to fire off while the car is on ice
+		onIce = true;
+		
+		StartCoroutine(slowDownTurnRand());
+		
+		//value of speed increase per frame while on ice
+		iceSpeedModifier = 0.4f;
+		
+		turningSpeed = 60;
+		
+		rigidbody.drag = 4;
+	}
+	
+	void OnTriggerExit()
+	{
+		onIce = false;
+		randomizeTurn = 0;
+		iceSpeedModifier = 0;
+		turningSpeed = 30;
+		drivingSpeed = 30;
 	}
 }
