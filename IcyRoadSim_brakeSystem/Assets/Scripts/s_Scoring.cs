d3e942e72af1203d3ae175650b7d13914b9e9884
Edward Kauffmann <ed@Eds-MacBook-Pro.local>
2013-11-11 21:20:50 -0600

    Driving Course
    I built a street for the driving course:
    
    The parked cars, lamps, buildings, and sidewalks are all triggers with
    their respective tags - when the player's car collides with them,
    they'd register that and change the score - i made a script with the
    basics of that, it just needs a way to communicate that info to the
    player at the end, when they enter the green "finish area"
    
    The icy patches can now be put over the road - also, feel free to add
    more cars or change the cars' positions to where you want to make the
    ice challenge.
    
    still no luck with the accelerator/brake pedals.  at this point, we
    might have to forget about them so that we can finish.

---
+++ IcyRoadSim_brakeSystem/Assets/Scripts/s_Scoring.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_Scoring : MonoBehaviour {
+	
+	public float ScoreTracker;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter (Collider c) {
+		
+		if (gameObject.tag == "ParkedCar") {
+			ScoreTracker = ScoreTracker-10f;
+		}
+		
+		if (gameObject.tag == "Lamp") {
+			ScoreTracker = ScoreTracker-8f;
+		}
+		
+		if (gameObject.tag == "Building") {
+			ScoreTracker = ScoreTracker-12f;
+		}
+		
+		if (gameObject.tag == "Sidewalk") {
+			ScoreTracker = ScoreTracker-6f;
+		}
+	}
+}
